0e0bc45dadbe4ba81b206f5d970cc0c70e685175
MaxRoenco <maxroenco2004@gmail.com>
2025-01-13 13:35:58 +0200

    main page almost fixed

--- quant-app/next.config.ts
+++ quant-app/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;

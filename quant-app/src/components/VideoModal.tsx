09fa1a5ad1ed2427fe961666f1f1b30faa6f52b5
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-01-17 11:13:59 +0200

    video modal fix

--- quant-app/src/components/VideoModal.tsx
+++ quant-app/src/components/VideoModal.tsx
@@ -2,29 +2,29 @@
 import { Modal } from '@mui/material';
 
 
-const VideoModal = ({ open, setOpen, handleClose, src, noSupportMessage } : any) => {
-  
+const VideoModal = ({ open, setOpen, handleClose, src, noSupportMessage }: any) => {
+
   return (
     <Modal
       open={open}
       onClose={handleClose}
+      id="video-popup"
+      className="modal d-flex"
     >
-      <div className="modal d-flex" id="video-popup" onClick={()=>console.log("CLICKEDD")}>
-        <div className="modal-content">
-          <span className="close-modal" onClick={handleClose} />
+      <div className="modal-content">
+        <span className="close-modal" onClick={handleClose} />
 
-          <video
-            id="localVideo"
-            width="100%"
-            controls
-            key={src}
-            autoPlay
-          >
-            <source src={src} type="video/mp4" />
+        <video
+          id="localVideo"
+          width="100%"
+          controls
+          key={src}
+          autoPlay
+        >
+          <source src={src} type="video/mp4" />
 
-            {noSupportMessage}
-          </video>
-        </div>
+          {noSupportMessage}
+        </video>
       </div>
     </Modal>
   );

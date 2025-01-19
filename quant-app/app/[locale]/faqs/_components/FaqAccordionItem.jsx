9fd2c5ac1fbeae10ef31273c951cc6187011e155
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-01-19 15:00:54 +0200

    removed useMemo

--- quant-app/app/[locale]/faqs/_components/FaqAccordionItem.jsx
+++ quant-app/app/[locale]/faqs/_components/FaqAccordionItem.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useMediaQuery } from "@mui/material";
 
 const FaqAccordionItem = (props) => {
@@ -10,33 +10,29 @@ const FaqAccordionItem = (props) => {
     setActive((prevActive) => !prevActive);
   };
 
-  const component = useMemo(() => {
-    return (
-      <div className={`faq__accordion-item ${active ? "active" : ""}`}>
-        <div
-          className="faq__accordion-header"
-          data-toggle="#ques1"
-          onClick={() => onClickToggle()}
-        >
-          <h2>{props.header}</h2>
-          <span className="faq__accordion-close"></span>
-        </div>
-        <div
-          className="faq__accordion-content"
-          id="ques1"
-          style={
-            isMobileScreen
-              ? { display: active ? "block" : "none" }
-              : { maxHeight: active ? "20rem" : "0" }
-          }
-        >
-          {props.content}
-        </div>
+  return (
+    <div className={`faq__accordion-item ${active ? "active" : ""}`}>
+      <div
+        className="faq__accordion-header"
+        data-toggle="#ques1"
+        onClick={() => onClickToggle()}
+      >
+        <h2>{props.header}</h2>
+        <span className="faq__accordion-close"></span>
       </div>
-    );
-  }, [active, props.header, props.content]);
-
-  return component;
+      <div
+        className="faq__accordion-content"
+        id="ques1"
+        style={
+          isMobileScreen
+            ? { display: active ? "block" : "none" }
+            : { maxHeight: active ? "20rem" : "0" }
+        }
+      >
+        {props.content}
+      </div>
+    </div>
+  );
 };
 
 export default FaqAccordionItem;

4fa9eeac40654efc6574a779d15752e3687e941c
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-01-19 17:46:14 +0200

    idk

--- quant-app/components/ScrollLink.tsx
+++ quant-app/components/ScrollLink.tsx
@@ -1,18 +1,25 @@
 'use client'
 import AppLink from "./AppLink"
 import { scrollToId } from "@/lib/utils/utils"
-export default function ScrollLink({ id, href, elementId, children }: any) {
-    return (
-        <span>
-            <AppLink
-                data-id={id}
-                href={href}
-                scroll={false}
-                onClick={()=>scrollToId(elementId)}
-            >
-                {children}
-            </AppLink>
-        </span>
 
-    )
+interface ScrollLinkProps {
+  id: string;
+  href: string;
+  elementId: string;
+  children: React.ReactNode;
+}
+
+export default function ScrollLink({ id, href, elementId, children }: ScrollLinkProps) {
+  return (
+    <span>
+      <AppLink
+        data-id={id}
+        href={href}
+        scroll={false}
+        onClick={() => scrollToId(elementId)}
+      >
+        {children}
+      </AppLink>
+    </span>
+  )
 }

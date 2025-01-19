d463913e18eff08fb4c015ce8bac44449aa2c47d
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-01-19 19:52:33 +0200

    test

--- quant-app/next.config.ts
+++ quant-app/next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 };
 
 export default withNextIntl(nextConfig);

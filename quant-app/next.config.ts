9a32daa367766aa75b6b674223e12d1261ff31ed
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-01-19 20:13:31 +0200

    cleanup

--- quant-app/next.config.ts
+++ quant-app/next.config.ts
@@ -8,12 +8,8 @@ const nextConfig: NextConfig = {
         ignoreDuringBuilds: true,
     },
     typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
         ignoreBuildErrors: true,
-      },
+    },
 };
 
 export default withNextIntl(nextConfig);

c6f16166b3948b15099d5ba99e7b46089aca7035
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-02-04 20:05:42 +0200

    refactor: reorganize component structure in page.tsx for improved readability

--- quant-app/app/[locale]/(main)/_components/Introduction.tsx
+++ quant-app/app/[locale]/(main)/_components/Introduction.tsx
@@ -11,113 +11,68 @@ export default async function Introduction() {
     const mobileIntro = getIntroByLanguageMobile(currentLocale);
 
     return (
-        <>
-            <div className="pt-[150px] max-[950px]:pt-[100px]">
-                <div className="banner__content">
-                    <div className="banner__text">
-                        <Typography
-                            variant="h1"
-                            className="!font-inter"
-                            sx={{
-                                fontSize: "55px",
-                                fontWeight: "600",
-                                lineHeight: "74px",
-                                color: "white",
-                                ":lang(ru)": {
-                                    fontSize: "48px",
-                                    lineHeight: "55px",
-                                },
-                                "@media (max-width: 900px)": {  // md breakpoint
-                                    fontSize: "40px",
-                                    ":lang(ru)": {
-                                        fontSize: "40px",
-                                    },
-                                    lineHeight: "50px",
-                                },
-                                "@media (max-width: 600px)": {  // sm breakpoint
-                                    fontSize: "30px",
-                                    ":lang(ru)": {
-                                        fontSize: "25px",
-                                    },
-                                    lineHeight: "40px",
-                                }
-                            }}
-                        >
-                            {t("WeBuildAppsThatEmpower")}
-                        </Typography>
-
-                        <Typography
-                            variant="h2"
-                            className="!font-inter"
-                            sx={{
-                                fontSize: "24px",
-                                lineHeight: "35px",
-                                color: "#EBEBEB",
-                                textAlign: "justify",
-                                "@media (max-width: 900px)": {
-                                    fontSize: "24px",
-                                    lineHeight: "30px",
-                                    textAlign: "center",
-                                },
-                                "@media (max-width: 600px)": {
-                                    fontSize: "18px",
-                                    lineHeight: "25px",
-                                    textAlign: "center",
-                                }
-                            }}
-                        >
-                            {t("WeSpecializeInBuilding")}
-                        </Typography>
-                    </div>
-
-                    <Box
+        <div className="pt-[150px] w-full p-20 max-[950px]:p-5 max-[950px]:pt-[100px]">
+            <div className="banner__content">
+                <div className="banner__text">
+                    <Typography
+                        variant="h1"
+                        className="!font-inter max-[1100px]:text-center"
                         sx={{
-                            mt: "0px",
-                            display: { xs: "flex", md: "none" }, // Показать только на md и больше
-                            justifyContent: "center",
+                            fontSize: "55px",
+                            fontWeight: "600",
+                            lineHeight: "74px",
+                            color: "white",
+                            ":lang(ru)": {
+                                fontSize: "48px",
+                                lineHeight: "55px",
+                            },
+                            "@media (max-width: 900px)": {  // md breakpoint
+                                fontSize: "40px",
+                                ":lang(ru)": {
+                                    fontSize: "40px",
+                                },
+                                lineHeight: "50px",
+                            },
+                            "@media (max-width: 600px)": {  // sm breakpoint
+                                fontSize: "30px",
+                                ":lang(ru)": {
+                                    fontSize: "25px",
+                                },
+                                lineHeight: "40px",
+                            }
                         }}
                     >
-                        <Link
-                            legacyBehavior
-                            href='/talk-modal'
-                        >
-                            <a className="button lets-talk fill-btn banner__text-btn">
-                                {t("LetsTalk")}
-                            </a>
-                        </Link>
-                    </Box>
+                        {t("WeBuildAppsThatEmpower")}
+                    </Typography>
 
-                    <div className="banner__video">
-                        <picture>
-                            <Image
-                                src="/images/ipad.webp"
-                                width={500}
-                                height={500}
-                                alt="Ipad"
-                                priority
-                                className="w-full h-full object-contain block"
-                            />
-                        </picture>
-                        <Link href="/video-modal" legacyBehavior>
-                            <a className="banner__video-play">
-                                <span />
-                                <p>{t("WatchVideo")}</p>
-                            </a>
-                        </Link>
-
-                        <Link href={mobileIntro} legacyBehavior>
-                            <a className="banner__video-play__mobile">
-                                <span></span>
-
-                                <p>{t("WatchVideo")}</p>
-                            </a>
-                        </Link>
-                    </div>
+                    <Typography
+                        variant="h2"
+                        className="!font-inter"
+                        sx={{
+                            fontSize: "24px",
+                            lineHeight: "35px",
+                            color: "#EBEBEB",
+                            textAlign: "justify",
+                            "@media (max-width: 900px)": {
+                                fontSize: "24px",
+                                lineHeight: "30px",
+                                textAlign: "center",
+                            },
+                            "@media (max-width: 600px)": {
+                                fontSize: "18px",
+                                lineHeight: "25px",
+                                textAlign: "center",
+                            }
+                        }}
+                    >
+                        {t("WeSpecializeInBuilding")}
+                    </Typography>
                 </div>
+
                 <Box
                     sx={{
-                        mt: "70px",
-                        display: { xs: "none", md: "flex" }, // Показать только на md и больше
+                        mt: "0px",
+                        display: { xs: "flex", md: "none" }, // Показать только на md и больше
                         justifyContent: "center",
                     }}
                 >
@@ -130,41 +85,84 @@ export default async function Introduction() {
                         </a>
                     </Link>
                 </Box>
-                <div className="overflow-hidden absolute top-0 left-0 w-full h-full -z-10">
-                    <Blur
-                        color="#1E50FF"
-                        left="-5%"
-                        top="0%"
-                        width="300px"
-                        height="300px"
-                        filter="blur(100px)"
-                    />
-                    <Blur
-                        color="#9C08FFE5"
-                        left="25%"
-                        top="0%"
-                        width="300px"
-                        height="300px"
-                        filter="blur(100px)"
-                    />
-                    <Blur
-                        color="#1E50FF"
-                        right="5%"
-                        top="10%"
-                        width="300px"
-                        height="300px"
-                        filter="blur(200px)"
-                    />
-                    <Blur
-                        color="#9C08FFE5"
-                        right="5%"
-                        top="0%"
-                        width="300px"
-                        height="300px"
-                        filter="blur(200px)"
-                    />
+
+                <div className="banner__video">
+                    <picture>
+                        <Image
+                            src="/images/ipad.webp"
+                            width={500}
+                            height={500}
+                            alt="Ipad"
+                            priority
+                            className="w-full h-full object-contain block"
+                        />
+                    </picture>
+                    <Link href="/video-modal" legacyBehavior>
+                        <a className="banner__video-play">
+                            <span />
+                            <p>{t("WatchVideo")}</p>
+                        </a>
+                    </Link>
+
+                    <Link href={mobileIntro} legacyBehavior>
+                        <a className="banner__video-play__mobile">
+                            <span></span>
+
+                            <p>{t("WatchVideo")}</p>
+                        </a>
+                    </Link>
                 </div>
             </div>
-        </>
+            <Box
+                sx={{
+                    mt: "70px",
+                    display: { xs: "none", md: "flex" }, // Показать только на md и больше
+                    justifyContent: "center",
+                }}
+            >
+                <Link
+                    legacyBehavior
+                    href='/talk-modal'
+                >
+                    <a className="button lets-talk fill-btn banner__text-btn">
+                        {t("LetsTalk")}
+                    </a>
+                </Link>
+            </Box>
+            <div className="overflow-hidden absolute top-0 left-0 w-full h-full -z-10">
+                <Blur
+                    color="#1E50FF"
+                    left="-5%"
+                    top="0%"
+                    width="300px"
+                    height="300px"
+                    filter="blur(100px)"
+                />
+                <Blur
+                    color="#9C08FFE5"
+                    left="25%"
+                    top="0%"
+                    width="300px"
+                    height="300px"
+                    filter="blur(100px)"
+                />
+                <Blur
+                    color="#1E50FF"
+                    right="5%"
+                    top="10%"
+                    width="300px"
+                    height="300px"
+                    filter="blur(200px)"
+                />
+                <Blur
+                    color="#9C08FFE5"
+                    right="5%"
+                    top="0%"
+                    width="300px"
+                    height="300px"
+                    filter="blur(200px)"
+                />
+            </div>
+        </div>
     )
 }
--- quant-app/app/[locale]/(main)/page.tsx
+++ quant-app/app/[locale]/(main)/page.tsx
@@ -18,25 +18,23 @@ const Page = async () => {
   return (
     <>
       <div className="">
-      <div className="container">
-        <Introduction /> 
+        <Introduction />
         <WhatWeOffer />
         <Box sx={{ mt: "50px", display: "flex", justifyContent: "center" }}>
           <Link href="/services" legacyBehavior>
             <button className="fill-btn">{t("ReadMore")}</button>
           </Link>
         </Box>
-      </div>
-      <QuantAppsSlider />
-      <section className="owr-work-section" id="projects">
-        <div className="container">
-          <Projects />
-          <Team />
-        </div>
-      </section>
-      <Vouches />
-      <NextProject />
-      {/* <div className="overflow-hidden absolute top-0 left-0 w-full h-full -z-10">
+        <QuantAppsSlider />
+        <section className="owr-work-section" id="projects">
+          <div className="container">
+            <Projects />
+            <Team />
+          </div>
+        </section>
+        <Vouches />
+        <NextProject />
+        {/* <div className="overflow-hidden absolute top-0 left-0 w-full h-full -z-10">
       <div className="absolute top-[15%] left-0 w-[600px] h-[600px] rounded-full bg-[rgba(186,8,255,0.8156512605042017)] blur-[140px] z-[-10]"/>
       </div> */}
       </div>

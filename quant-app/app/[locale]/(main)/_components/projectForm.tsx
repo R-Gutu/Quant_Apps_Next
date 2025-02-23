5235ca0f21670a8c7838c4d1e97dc8435b89d914
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-02-23 12:34:19 +0200

    form

--- quant-app/app/[locale]/(main)/_components/projectForm.tsx
+++ quant-app/app/[locale]/(main)/_components/projectForm.tsx
@@ -15,7 +15,7 @@ const schema = yup.object().shape({
     message: yup.string().required("Message is required").min(10, "Must be at least 10 characters"),
 });
 
-export default function ProjectForm() {
+export default function ProjectForm({ className }: { className?: string }) {
     const [budget, setBudget] = useState<number[]>([1000, 5000]);
 
     const { register, handleSubmit, control, formState: { errors }, setValue, watch } = useForm({
@@ -45,22 +45,23 @@ export default function ProjectForm() {
     };
 
     return (
-        <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-[40px] items-center w-full py-[100px] px-[20%]'>
+        <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className={`flex flex-col gap-[40px] items-center w-full py-[100px] px-[20%] max-[1100px]:px-[40px] ${className}`}>
             <div className='flex max-[1200px]:flex-col w-full gap-[40px]'>
-                <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px]'>
-                    <label htmlFor="name" className='text-[22px] text-white'>Full Name</label>
-                    <input id="name" {...register("name")} type="text" placeholder='Type here' className='appearance-none bg-transparent placeholder:text-[#656567] placeholder:text-[18px] border-b-[1px] border-[#333333] p-[6px] outline-none focus:placeholder:text-opacity-0 text-white' />
+                <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px] max-[600px]:px-[24px] max-[600px]:py-[18px]'>
+                    <label htmlFor="name" className='text-[22px] max-[600px]:text-[16px] text-white'>Full Name</label>
+                    <input id="name" {...register("name")} type="text" placeholder='Type here' className='appearance-none bg-transparent placeholder:text-[#656567] placeholder:text-[18px] max-[600px]:placeholder:text-[16px] border-b-[1px] border-[#333333] p-[6px] pl-0 outline-none focus:placeholder:text-opacity-0 text-white' />
                     {errors.name && <span className="text-red-500 text-sm">{errors.name.message}</span>}
                 </div>
-                <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px]'>
-                    <label htmlFor="email" className='text-[22px] text-white'>Email</label>
-                    <input id="email" {...register("email")} type="email" placeholder='Type here' className='appearance-none bg-transparent placeholder:text-[#656567] placeholder:text-[18px] border-b-[1px] border-[#333333] p-[6px] outline-none focus:placeholder:text-opacity-0 text-white' />
+                <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px] max-[600px]:px-[24px] max-[600px]:py-[18px]'>
+                    <label htmlFor="email" className='text-[22px] max-[600px]:text-[16px] text-white'>Email</label>
+                    <input id="email" {...register("email")} type="email" placeholder='Type here' className='appearance-none bg-transparent placeholder:text-[#656567] placeholder:text-[18px] max-[600px]:placeholder:text-[16px] border-b-[1px] border-[#333333] p-[6px] pl-0 outline-none focus:placeholder:text-opacity-0 text-white' />
                     {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
                 </div>
             </div>
-            <div className='text-white w-full flex flex-col gap-[40px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px]'>
-                <h2 className='text-[22px] text-white'>Select the services you need for your project:</h2>
-                <div className='grid grid-cols-2 gap-[24px]'>
+            <div className='text-white w-full flex flex-col gap-[40px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px] max-[600px]:px-[24px] max-[600px]:py-[18px]'>
+                <h2 className='text-[22px] text-white max-[750px]:hidden'>Select the services you need for your project:</h2>
+                <h2 className='hidden text-[22px] max-[600px]:text-[16px] text-white max-[750px]:block'>Why are you contacting us?</h2>
+                <div className='grid grid-cols-2 max-[750px]:grid-cols-1 gap-[24px]'>
                     {[
                         { id: "web-dev", label: "Web development" },
                         { id: "crm-dev", label: "CRM development" },
@@ -87,14 +88,14 @@ export default function ProjectForm() {
                                 />
                                 <Image className='peer-checked:opacity-100 pointer-events-none opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[-62%] transition-opacity duration-300' src='/images/icons/check.svg' width={20} height={20} alt='check mark' />
                             </div>
-                            <label htmlFor={id}>{label}</label>
+                            <label htmlFor={id} className="text-[18px] max-[600px]:text-[16px]">{label}</label>
                         </div>
                     ))}
                 </div>
                 {errors.services && <span className="text-red-500 text-sm">{errors.services.message}</span>}
             </div>
-            <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px]'>
-                <label className='text-[22px] text-white'>Your Budget</label>
+            <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px] max-[600px]:px-[24px] max-[600px]:py-[18px]'>
+                <label className='text-[22px] max-[600px]:text-[16px] text-white'>Your Budget</label>
                 <p className='font-normal text-[18px] text-[#E6E6E6] mb-[30px]'>Slide to indicate your budget range</p>
                 <Controller
                     name="budget"
@@ -113,9 +114,9 @@ export default function ProjectForm() {
                     )}
                 />
             </div>
-            <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px]'>
-                <label htmlFor="message" className='text-[22px] text-white'>Your Message</label>
-                <input type="text" id="message" {...register("message")} placeholder='Type here' rows={4} className='appearance-none bg-transparent placeholder:text-[#656567] placeholder:text-[18px] border-b-[1px] border-[#333333] p-[6px] outline-none focus:placeholder:text-opacity-0 text-white resize-none' />
+            <div className='w-full flex flex-col gap-[20px] border-[1px] border-solid border-[#6A65FF] rounded-[8px] py-[24px] px-[40px] max-[600px]:px-[24px] max-[600px]:py-[18px]'>
+                <label htmlFor="message" className='text-[22px] max-[600px]:text-[16px] text-white'>Your Message</label>
+                <input type="text" id="message" {...register("message")} placeholder='Type here' rows={4} className='appearance-none bg-transparent placeholder:text-[#656567] placeholder:text-[18px] max-[600px]:placeholder:text-[16px] border-b-[1px] border-[#333333] p-[6px] pl-0 outline-none focus:placeholder:text-opacity-0 text-white resize-none' />
                 {errors.message && <span className="text-red-500 text-sm">{errors.message.message}</span>}
             </div>
             <button type="submit" className='appearance-none outline-none border-none px-[44px] py-[18px] flex items-center justify-center text-white text-[18px] bg-[linear-gradient(89.13deg,_#836FFF_0.18%,_#4A5DE5_99.86%)] cursor-pointer btn'>

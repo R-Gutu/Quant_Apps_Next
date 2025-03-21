6304313cf9b376fd527b62667e980070a8bb3dd1
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-03-21 10:58:12 +0200

    Created formAlert

--- quant-app/components/FormAlert.tsx
+++ quant-app/components/FormAlert.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import { Modal } from "@mui/material";
-import { getIntroByLanguage } from "@/lib/utils/languageUtils";
-import { useLocale, useTranslations } from "next-intl";
 import Image from "next/image";
 import { useEffect } from "react";
 
-const VideoModal = ({ setIsOpen }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
-    const currentLocale = useLocale();  // ✅ Now inside a Client Component
-    const intro = getIntroByLanguage(currentLocale);
-    const t = useTranslations("video");
+const FormAlert = ({ setIsOpen }: { setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
 
     useEffect(() => {
         document.querySelector("body")?.classList.add("overflow-hidden");
@@ -27,7 +22,7 @@ const VideoModal = ({ setIsOpen }: { setIsOpen: React.Dispatch<React.SetStateAct
             onClose={handleOnClose}
             className="px-[10%] max-[1000px]:px-[20px] max-[700px]:px-[0px] flex justify-center items-center"
         >
-            <div className="bg-white p-[40px] max-[700px]:p-[20px] max-[700px]:pt-[40px] rounded-[15px] relative">
+            <div className="bg-white p-[40px] max-[700px]:p-[20px] max-[700px]:pt-[40px] rounded-[15px] relative flex flex-col justify-center items-center">
                 <Image
                     src="/images/icons/cross.svg"
                     alt="close cross"
@@ -36,13 +31,12 @@ const VideoModal = ({ setIsOpen }: { setIsOpen: React.Dispatch<React.SetStateAct
                     className="absolute top-[-20px] max-[700px]:top-[-30px] right-0 cursor-pointer"
                     onClick={handleOnClose}
                 />
-                <video width="100%" controls key={intro} autoPlay preload="none">
-                    <source src={intro} type="video/mp4" />
-                    {t("notSupported")}
-                </video>
+                <Image src="/images/icons/logo_no_text.svg" alt="logo" width={106} height={106} className="pb-[24px]" />
+                <p className="font-semibold text-[24px] pb-[40px] text-center">We've received your message! Our team will get back to you shortly.</p>
+                <button onClick={handleOnClose} className="bg-[linear-gradient(90deg,_#8D139B_-111.43%,_#841BA1_-78.76%,_#6D32B1_-28.79%,_#4958CC_34.63%,_#2979E4_80.76%)] w-[70%] px-[20px] py-[14px] rounded-[8px] text-white font-medium text-[16px]">Ok</button>
             </div>
         </Modal>
     );
 };
 
-export default VideoModal;
+export default FormAlert;

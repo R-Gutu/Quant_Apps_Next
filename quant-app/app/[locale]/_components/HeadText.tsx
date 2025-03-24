7e6c01078b4a770a56bb45850994dbe93d1056d1
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-03-24 13:03:48 +0200

    Made sure all pages have a single h1 tag

--- quant-app/app/[locale]/_components/HeadText.tsx
+++ quant-app/app/[locale]/_components/HeadText.tsx
@@ -1,7 +1,7 @@
 const HeadText = ({title, text}: {title: string, text: string}) => {
   return (
     <div className="p-10 flex flex-col gap-[16px]">
-        <h1 className="font-semibold text-[48px] max-mui-md:text-[40px] max-smallest:text-[26px] leading-[58.09px] tracking-[0%] text-[#FFFFFF]">{title}</h1>
+        <p className="font-semibold text-[48px] max-mui-md:text-[40px] max-smallest:text-[26px] leading-[58.09px] tracking-[0%] text-[#FFFFFF]">{title}</p>
         <p className="font-normal text-[18px] leading-[27px] max-mui-md:text-[16px] max-smallest:text-[14px] tracking-[-0.6%] text-[#E6E6E6] opacity-75">{text}</p>
     </div>
   )

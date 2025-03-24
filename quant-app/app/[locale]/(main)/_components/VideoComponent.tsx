853c2636c9abfece31448a268bf41fd195bf4a25
MaxRoenco <maxroenco2004@gmail.com>
2025-03-24 15:11:24 +0200

    deleted unoptimized

--- quant-app/app/[locale]/(main)/_components/VideoComponent.tsx
+++ quant-app/app/[locale]/(main)/_components/VideoComponent.tsx
@@ -10,7 +10,7 @@ export default function VideoComponent({ ipadAlt, watchAlt } : { ipadAlt: string
         <>
             <div className="flex items-center justify-center relative mt-[25px] w-full">
                 <Image src="/images/pictures/ipad.webp" priority width={578} height={443} alt={ipadAlt} className="h-auto w-[85%] min-[700px]:w-[530px] min-[1900px]:w-[578px]" />
-                <Image onClick={() => setVideoOpen(true)} priority className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[30%] duration-200 transition-all cursor-pointer hover:scale-110 hover:drop-shadow-2xl" unoptimized src="/images/elements/watch.svg" width={261} height={120} alt={watchAlt} />
+                <Image onClick={() => setVideoOpen(true)} priority className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[30%] duration-200 transition-all cursor-pointer hover:scale-110 hover:drop-shadow-2xl" src="/images/elements/watch.svg" width={261} height={120} alt={watchAlt} />
                 <Blur
                     color='#4A5DE5'
                     left="200px"

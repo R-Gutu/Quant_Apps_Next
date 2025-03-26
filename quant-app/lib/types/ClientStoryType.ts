f490bbbba53143090a5b7a4b9302b826f50a0ffa
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-03-26 12:33:28 +0200

    cleaned up unused files and dependencies

--- quant-app/lib/types/ClientStoryType.ts
+++
@@ -1,5 +0,0 @@
-export default interface ClientStoryType {
-    clientName: string,
-    clientDescription?: string,
-    text: string
-  }
--- quant-app/lib/utils/languageUtils.ts
+++ quant-app/lib/utils/languageUtils.ts
@@ -8,37 +8,3 @@ export function getIntroByLanguage(language: string) {
       return '/en/videos/intro-en.mp4';
   }
 }
-
-export function getIntroByLanguageMobile(language: string) {
-  switch (language) {
-    case "ro":
-      return 'ro/videos/intro-ro.mp4';
-    case "ru":
-      return 'ru/videos/intro-ru.mp4';
-    default:
-      return 'en/videos/intro-en.mp4';
-  }
-}
-
-export function getRoadMapByLanguage(language: string) {
-  switch (language) {
-    case "ru":
-      return {
-        desktop: "/icons/road-map-ru.svg",
-        mobileLeft: "/icons/road-map-ru-left.svg",
-        mobileRight: "/icons/road-map-ru-right.svg",
-      };
-    case "ro":
-      return {
-        desktop: "/icons/road-map-ro.svg",
-        mobileLeft: "/icons/road-map-ro-left.svg",
-        mobileRight: "/icons/road-map-ro-right.svg",
-      };
-    default:
-      return {
-        desktop: "/icons/road-map-en.svg",
-        mobileLeft: "/icons/road-map-en-left.svg",
-        mobileRight: "/icons/road-map-en-right.svg",
-      };
-  }
-}

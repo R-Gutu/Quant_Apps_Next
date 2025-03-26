b418a4efbdd4d5ce9684b2799c4c944a2b95d82a
IslamAbukoush <islam.abu.koush@isa.utm.md>
2025-03-26 14:40:02 +0200

    added tags to utils

--- quant-app/lib/utils/utils.tsx
+++ quant-app/lib/utils/utils.tsx
@@ -1,9 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { ReactNode } from "react"
 import { twMerge } from "tailwind-merge"
-
+import { Link } from "@/i18n/routing"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const strong = { strong: (chunck : ReactNode) => <strong>{chunck}</strong> }
+export const strong = {
+  strong: (chunck: ReactNode) => <strong>{chunck}</strong>,
+  services: (chunck: ReactNode) => <Link href={'/services'} className="cursor-pointer">{chunck}</Link>,
+  careers: (chunck: ReactNode) => <Link href={'/careers'} className="cursor-pointer">{chunck}</Link>,
+  aboutUs: (chunck: ReactNode) => <Link href={'/about-us'} className="cursor-pointer">{chunck}</Link>,
+  projects: (chunck: ReactNode) => <Link href={'/projects'} className="cursor-pointer">{chunck}</Link>,
+  process: (chunck: ReactNode) => <Link href={'/process'} className="cursor-pointer">{chunck}</Link>,
+}
